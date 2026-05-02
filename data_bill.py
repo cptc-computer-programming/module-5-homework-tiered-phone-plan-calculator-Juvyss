afea02da62e5cfb361e9fa90ab0d341e8767ab0b
Cody Brown <cody.thecodeman71@gmail.com>
2026-05-02 02:16:33 -0700

    Fixxed the formating, boolean, and comparison
    Program was not working properly (bugged). I fixed it by rearranging the rational operators and taking the 'or' out of the Boolean. I also fixed the format to look nicer to my eyes

--- data_bill.py
+++ data_bill.py
@@ -10,9 +10,11 @@ REGULAR_USER_OVERAGE_RATE_TIER_3 = 3
 
 # Your code goes here:
 
+
 # Welcome message
 
 print("Hi! I am data_bill, I will calculate your phone bill for you. Please respond to prompts below:")
+
 
 # User Inputs
 
@@ -20,9 +22,11 @@ monthly_data_useage = float(input("Please tell me how much data you have used si
 base_plan_cost = float(input("Please tell me the cost of you plan per month: "))
 premium_user = input("Please tell me if you a premium account holder (yes or no): ")
 
+
 # Boolean for premium
 
-has_premium = premium_user == 'yes' or 'Yes'
+has_premium = premium_user == "yes"
+
 
 # Logic to determine their charges
 
@@ -30,27 +34,28 @@ overage_gb = monthly_data_useage - TIER_1_DATA_LIMIT_GB
 overage_rate = ''
 
 if has_premium:
-    if monthly_data_useage >= TIER_2_DATA_LIMIT_GB:
+    if monthly_data_useage > TIER_2_DATA_LIMIT_GB:
         overage_cost = overage_gb * PREMIUM_USER_OVERAGE_RATE_TIER_3
         total = base_plan_cost + overage_cost
         overage_rate = PREMIUM_USER_OVERAGE_RATE_TIER_3
-    elif monthly_data_useage < TIER_2_DATA_LIMIT_GB and monthly_data_useage >= TIER_1_DATA_LIMIT_GB:
+    elif monthly_data_useage <= TIER_2_DATA_LIMIT_GB and monthly_data_useage > TIER_1_DATA_LIMIT_GB:
         overage_cost = overage_gb * PREMIUM_USER_OVERAGE_RATE_TIER_2
         total = base_plan_cost + overage_cost
-        overage_rate = PREMIUM_USER_OVERAGE_RATE_TIER_3
+        overage_rate = PREMIUM_USER_OVERAGE_RATE_TIER_2
     else:
         total = base_plan_cost
 else:
-    if monthly_data_useage >= TIER_2_DATA_LIMIT_GB:
+    if monthly_data_useage > TIER_2_DATA_LIMIT_GB:
         overage_cost = overage_gb * REGULAR_USER_OVERAGE_RATE_TIER_3
         total = base_plan_cost + overage_cost
         overage_rate = REGULAR_USER_OVERAGE_RATE_TIER_3
-    elif monthly_data_useage < TIER_2_DATA_LIMIT_GB and monthly_data_useage >= TIER_1_DATA_LIMIT_GB:
+    elif monthly_data_useage <= TIER_2_DATA_LIMIT_GB and monthly_data_useage > TIER_1_DATA_LIMIT_GB:
         overage_cost = overage_gb * REGULAR_USER_OVERAGE_RATE_TIER_2
         total = base_plan_cost + overage_cost
         overage_rate = REGULAR_USER_OVERAGE_RATE_TIER_2
     else:
         total = base_plan_cost
+
 
 # Our Print Statement
 
@@ -58,7 +63,7 @@ if monthly_data_useage > TIER_1_DATA_LIMIT_GB:
     print("You are over your limit by ", overage_gb, " GB.")
     print("Overage rate: $", format(overage_rate, ".02f"), sep = ' ')
     print("Overage cost: $", format(overage_cost, ".02f"), sep = ' ')
-    print("Your total bill is $", format(total, ".02f"), sep = ' ')
+    print("Your total bill is $", format(total, ".02f"), sep = ' ')   
 else:
     print("You are within your data limit.")
     print("Your total bill is $", format(total, ".02f"), sep = ' ')
